1117149abbeb1f4cbb13dc67c3162645e7a9a075
ElenaMikhaleva <elena.mikhaleva@gmail.com>
2025-06-20 15:05:02 +0300

    improved registration tests with random generator

--- src/test/java/ui_tests/LogInTests.java
+++ src/test/java/ui_tests/LogInTests.java
@@ -2,18 +2,25 @@ package ui_tests;
 
 import dto.User;
 import manager.ApplicationManager;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.ContactsPage;
 import pages.HomePage;
 import pages.LoginPage;
 
+import java.util.Random;
+
 public class LogInTests extends ApplicationManager {
+
+    public String emailGenerator() {
+        Random random = new Random();
+        int number = random.nextInt(1000); // Any number between 0 and 999
+        return "user" + number + "@test.com";
+    }
 
     @Test
     public void regPositiveTest() {
-        User user = new User("elena12@gmail.com", "Password$1");
+        User user = new User(emailGenerator(), "Password$1");
         HomePage homePage = new HomePage(getDriver());
         homePage.clickBtnHeaderLogin();
         LoginPage loginPage = new LoginPage(getDriver());
